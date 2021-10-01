c65ac21b096dfe3fcbc9dbbbbc01cc29ea4a13bd
Selin Karademir <47951360+selinkarademir@users.noreply.github.com>
2021-10-01 13:10:03 +0300

    Create DirectorRepository.cs

---
+++ src/dbContext/Repository/Base/DirectorRepository.cs
@@ -0,0 +1,16 @@
+using dbContext.Entities;
+using dbContext.Repository.Context;
+using dbContext.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbContext.Repository.Base
+{
+  public class DirectorRepository:BaseRepository<director, NPGContext>,IDirectorRepository
+    {
+    
+    }
+}
