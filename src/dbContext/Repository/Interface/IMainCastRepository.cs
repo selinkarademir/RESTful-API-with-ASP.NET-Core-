553b1b4363838a8108dc748fa0c47b87833480f9
Selin Karademir <47951360+selinkarademir@users.noreply.github.com>
2021-10-01 13:13:36 +0300

    Create IMainCastRepository.cs

---
+++ src/dbContext/Repository/Interface/IMainCastRepository.cs
@@ -0,0 +1,14 @@
+using dbContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbContext.Repository.Interface
+{
+    public interface IMainCastRepository:IRepository<maincast>
+    {
+
+    }
+}
