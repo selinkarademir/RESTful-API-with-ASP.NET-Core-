ca58699c99d1c6eadf553a7c2acdee872d5ff6ad
Selin Karademir <47951360+selinkarademir@users.noreply.github.com>
2021-10-01 13:13:53 +0300

    Create IGenreRepository.cs

---
+++ src/dbContext/Repository/Interface/IGenreRepository.cs
@@ -0,0 +1,13 @@
+using dbContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbContext.Repository.Interface
+{
+   public interface IGenreRepository: IRepository<genre>
+    {
+    }
+}
